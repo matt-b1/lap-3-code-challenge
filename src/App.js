a40d9ab5cba63b890b71137199d0d6129364ae06
jakerinahmed <jakerin.ahmed2001@gmail.com>
2022-10-05 09:57:51 +0100

    add Home and About pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import {Header} from './Layout'
+import {Home, About } from './pages'
 
 function App() {
   return (
     <div>
       <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur pariatur reprehenderit ut hic officia unde quo molestiae placeat. Dolorem deserunt consectetur saepe omnis sequi, voluptatem culpa id eos numquam possimus sunt commodi nesciunt, asperiores non vero eius incidunt sint voluptatibus quasi veritatis dignissimos. Unde quod libero odit voluptas asperiores, ratione dolores nulla officia, sint culpa consequuntur minima eum tempore labore adipisci repudiandae deleniti voluptatem maiores minus ducimus esse in similique. Aut inventore sequi vel praesentium ipsum officiis, dicta ea minima sunt odio facilis expedita, debitis laboriosam. Mollitia pariatur error aliquid animi quas atque distinctio molestiae beatae, maxime est aperiam minima in rem id, adipisci voluptates ex dicta velit repudiandae, iure illum nobis hic cumque. Expedita consectetur, odio provident earum unde numquam consequuntur tenetur, eligendi similique qui ipsam aliquam assumenda mollitia. Reprehenderit suscipit iusto incidunt placeat animi eligendi, autem alias numquam quibusdam asperiores ex amet reiciendis, facilis, magnam magni voluptates voluptatum quisquam molestias tenetur nihil fugiat iure rem hic? Nobis labore molestiae modi impedit voluptatibus. Recusandae dicta cumque nobis maxime enim. Quidem minus optio rem eveniet, saepe dolores corporis voluptates suscipit. Ut id aut vero facilis deleniti maiores voluptas ducimus veniam aperiam libero, sit vitae earum rem consequatur voluptate debitis quas!</p>
+    </div>
+  )
+}
+
+export default About
